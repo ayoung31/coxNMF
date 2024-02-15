c6344d4b29a04f64c97c9ad6b23907e8963a696d
ayoung31 <54555484+ayoung31@users.noreply.github.com>
2024-02-15 16:15:51 -0500

    Add function for random initializations

---
+++ R/init.R
@@ -0,0 +1,18 @@
+#' @export
+init <- function(X,y,delta,k,alpha,ninit=10,maxit=15){
+  p <- nrow(X)
+  n <- ncol(X)
+  loss_best <- 1/0
+  for(i in 1:ninit){
+    H0 <- matrix(runif(n*k,0,1),nrow=k)
+    W0 <- matrix(runif(p*k,0,1),nrow=p)
+    beta0 <- runif(k,-1,1)
+    fit <- optimize_loss(X,H0,W0,beta0,y,delta,alpha=10e-2,maxit=maxit)
+    loss <- fit$loss
+    if(loss < loss_best){
+      loss_best=lost
+      fit_best=fit
+    }
+  }
+  return(list(W0=fit_best$W,H0=fit_best$H,beta0=fit_best$beta))
+}
