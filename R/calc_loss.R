86e2067f50a3e1d0d1a67b3fe68cd6d6684f37ba
ayoung31 <54555484+ayoung31@users.noreply.github.com>
2024-03-07 10:41:55 -0500

    Fix survival component of loss

--- R/calc_loss.R
+++ R/calc_loss.R
@@ -9,6 +9,6 @@ calc_loss <- function(X,W,H,beta,alpha,y,delta,lambda=1,eta=0){
       delta[i]*(t(beta)%*%H[,i] - 
                   log(sum((y>=y[i])*t(exp(t(beta)%*%H)))))
   }
-  loss <- nmf_loss - alpha*surv_loss
+  loss <- nmf_loss + alpha*(-2*surv_loss/N + lambda*(((1-eta)/2)*sum(beta^2) + eta*sum(abs(beta))))
   return(list(loss=loss,nmf_loss=nmf_loss,surv_loss=surv_loss))
 }
