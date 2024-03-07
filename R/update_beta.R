03975962772e651fe5b8b9b5c642a47d25593446
ayoung31 <54555484+ayoung31@users.noreply.github.com>
2024-03-07 10:44:38 -0500

    standardize=T

--- R/update_beta.R
+++ R/update_beta.R
@@ -3,7 +3,7 @@ update_beta <- function(H,y,delta,lambda=0,eta=1){
   
   y_surv <- survival::Surv(y,delta)
   fit <- glmnet::glmnet(t(H),y_surv,family='cox',
-                        lambda=lambda,alpha=eta,standardize = FALSE)
+                        lambda=lambda,alpha=eta)
   
   return(as.numeric(coef(fit)))
 }
