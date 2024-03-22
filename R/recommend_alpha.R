a3905bed5215d374338331f243b9772e573c9705
ayoung31 <54555484+ayoung31@users.noreply.github.com>
2024-03-22 17:01:22 -0400

    create function to recommend balancing parameter

---
+++ R/recommend_alpha.R
@@ -0,0 +1,17 @@
+recommend_alpha <- function(X,y,delta,k,nalpha,maxit=15){
+  if(nalpha < 1){
+    warning('nalpha must be a positive integer')
+  }
+  H0 <- matrix(runif(n*k,0,1),nrow=k)
+  W0 <- matrix(runif(p*k,0,1),nrow=p)
+  beta0 <- rep(0,k)
+  fit0 <- optimize_loss(X,H0,W0,beta0,y,delta,alpha=0,maxit=maxit)
+  alpha5050 <- fit0$nmf_loss / fit0$surv_loss
+  if(nalpha %% 2 > 0){
+    nalpha=nalpha+1
+  }
+  grid <- 2:(nalpha/2 + 1)
+  alpha_grid <- c(alpha5050*grid,alpha5050,alpha5050/grid)
+  
+  return(list(alpha5050=alpha5050, alpha_grid=alpha_grid))
+}
