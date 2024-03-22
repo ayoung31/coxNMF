e7d623e37534db0ea71f05e7187807a19c36ac4a
ayoung31 <54555484+ayoung31@users.noreply.github.com>
2024-03-22 16:40:40 -0400

    Add ramp up to initialization

--- R/init.R
+++ R/init.R
@@ -1,5 +1,9 @@
 #' @export
 init <- function(X,y,delta,k,alpha,ninit=10,maxit=15){
+  
+  if(maxit <10){
+    warning('Maxit must be at least 10')
+  }
   p <- nrow(X)
   n <- ncol(X)
   loss_best <- 1/0
@@ -8,7 +12,8 @@ init <- function(X,y,delta,k,alpha,ninit=10,maxit=15){
     W0 <- matrix(runif(p*k,0,1),nrow=p)
     #beta0 <- runif(k,-1,1)
     beta0 <- rep(0,k)
-    fit <- optimize_loss(X,H0,W0,beta0,y,delta,alpha=alpha,maxit=maxit)
+    fit0 <- optimize_loss(X,H0,W0,beta0,y,delta,alpha=0,maxit=10)
+    fit <- optimize_loss(X,fit0$H,fit0$W,fit0$beta,y,delta,alpha=alpha,maxit=max(maxit-10,0))
     loss <- fit$loss
     if(loss < loss_best){
       loss_best=loss
