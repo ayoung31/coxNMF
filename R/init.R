baaf8d621b7974184c026acbcef1ae02e82989ef
ayoung31 <54555484+ayoung31@users.noreply.github.com>
2024-05-06 14:30:06 -0400

    debug

--- R/init.R
+++ R/init.R
@@ -1,5 +1,5 @@
 #' @export
-init <- function(X,M,y,delta,k,alpha,WtX,lambda,eta,ninit=5,imaxit=10,warmup=1,...){
+init <- function(X,M,y,delta,k,alpha,WtX,lambda,eta,ninit=5,imaxit=10,warmup=1,verbose=verbose){
   
   if(warmup >= imaxit){
     warning('Maxit must be larger than warmup')
@@ -13,9 +13,9 @@ init <- function(X,M,y,delta,k,alpha,WtX,lambda,eta,ninit=5,imaxit=10,warmup=1,.
     #beta0 <- runif(k,-1,1)
     beta0 <- rep(0,k)
     fit0 <- optimize_loss(X=X,M=M,H0=H0,W0=W0,beta0=beta0,y=y,delta=delta,
-                          alpha=0,lambda=0,eta=0,maxit=warmup,WtX=WtX,...)
+                          alpha=0,lambda=0,eta=0,maxit=warmup,WtX=WtX,verbose=verbose,...)
     fit <- optimize_loss(X=X,M=M,H0=fit0$fit$H,W0=fit0$fit$W,beta0=fit0$fit$beta,y=y,delta=delta,
-                         alpha=alpha,lambda=lambda,eta=eta,maxit=max(imaxit-warmup,0),WtX=WtX,...)
+                         alpha=alpha,lambda=lambda,eta=eta,maxit=max(imaxit-warmup,0),WtX=WtX,verbose=verbose)
     loss <- fit$loss$loss
     if(loss < loss_best){
       loss_best=loss
