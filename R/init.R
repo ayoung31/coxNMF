e6363b34231ecc8c5ddcf7a11effe00767c5a82f
ayoung31 <54555484+ayoung31@users.noreply.github.com>
2024-05-06 14:34:33 -0400

    debug

--- R/init.R
+++ R/init.R
@@ -13,7 +13,7 @@ init <- function(X,M,y,delta,k,alpha,WtX,lambda,eta,ninit=5,imaxit=10,warmup=1,v
     #beta0 <- runif(k,-1,1)
     beta0 <- rep(0,k)
     fit0 <- optimize_loss(X=X,M=M,H0=H0,W0=W0,beta0=beta0,y=y,delta=delta,
-                          alpha=0,lambda=0,eta=0,maxit=warmup,WtX=WtX,verbose=verbose,...)
+                          alpha=0,lambda=0,eta=0,maxit=warmup,WtX=WtX,verbose=verbose)
     fit <- optimize_loss(X=X,M=M,H0=fit0$fit$H,W0=fit0$fit$W,beta0=fit0$fit$beta,y=y,delta=delta,
                          alpha=alpha,lambda=lambda,eta=eta,maxit=max(imaxit-warmup,0),WtX=WtX,verbose=verbose)
     loss <- fit$loss$loss
