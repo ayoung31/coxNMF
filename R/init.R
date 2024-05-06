9ba599f7e528b5be9cb04aa6feeed43f38b0eae2
ayoung31 <54555484+ayoung31@users.noreply.github.com>
2024-05-06 16:15:34 -0400

    debug

--- R/init.R
+++ R/init.R
@@ -1,5 +1,5 @@
 #' @export
-init <- function(X,M,y,delta,k,alpha,WtX,lambda,eta,ninit=5,imaxit=10,warmup=1,verbose=verbose){
+init <- function(X,M,y,delta,k,alpha,WtX,lambda,eta,ninit=5,imaxit=10,warmup=1,verbose,norm.type){
   
   if(warmup >= imaxit){
     warning('Maxit must be larger than warmup')
@@ -8,14 +8,14 @@ init <- function(X,M,y,delta,k,alpha,WtX,lambda,eta,ninit=5,imaxit=10,warmup=1,v
   n <- ncol(X)
   loss_best <- 1/0
   for(i in 1:ninit){
-    H0 <- matrix(runif(n*k,0,1),nrow=k)
-    W0 <- matrix(runif(p*k,0,1),nrow=p)
+    H0 <- matrix(runif(n*k,0,max(X)),nrow=k)
+    W0 <- matrix(runif(p*k,0,max(X)),nrow=p)
     #beta0 <- runif(k,-1,1)
     beta0 <- rep(0,k)
     fit0 <- optimize_loss(X=X,M=M,H0=H0,W0=W0,beta0=beta0,y=y,delta=delta,
-                          alpha=0,lambda=0,eta=0,maxit=warmup,WtX=WtX,verbose=verbose)
+                          alpha=0,lambda=0,eta=0,maxit=warmup,WtX=WtX,verbose=verbose,norm.type=norm.type)
     fit <- optimize_loss(X=X,M=M,H0=fit0$fit$H,W0=fit0$fit$W,beta0=fit0$fit$beta,y=y,delta=delta,
-                         alpha=alpha,lambda=lambda,eta=eta,maxit=max(imaxit-warmup,0),WtX=WtX,verbose=verbose)
+                         alpha=alpha,lambda=lambda,eta=eta,maxit=max(imaxit-warmup,0),WtX=WtX,verbose=verbose,norm.type=norm.type)
     loss <- fit$loss$loss
     if(loss < loss_best){
       loss_best=loss
--- R/run_coxNMF.R
+++ R/run_coxNMF.R
@@ -1,10 +1,11 @@
 #' @export
-run_coxNMF = function(dat, k, alpha, lambda, eta, WtX,verbose=TRUE,...){
+run_coxNMF = function(dat, k, alpha, lambda, eta, WtX,verbose=TRUE,norm.type='none',...){
   # now do survival
-  fit0 = init(X = dat$X,M=dat$M,y = dat$s[,1],delta = dat$s[,2],k = k,alpha = alpha, lambda=lambda, eta=eta, WtX=WtX, verbose=verbose, ...) # new init adjustment here
+  fit0 = init(X = dat$X,M=dat$M,y = dat$s[,1],delta = dat$s[,2],k = k,alpha = alpha, 
+              lambda=lambda, eta=eta, WtX=WtX, verbose=verbose, norm.type=norm.type,...) # new init adjustment here
   fit = optimize_loss(X = dat$X, dat$M, y = dat$s[,1],delta = dat$s[,2],H0=fit0$H0,
                       W0 = fit0$W0,beta0=fit0$beta0, alpha = alpha,lambda = lambda, 
-                      eta = eta, WtX=WtX,verbose=verbose)
+                      eta = eta, WtX=WtX,verbose=verbose,norm.type=norm.type)
   
   return(fit)
 }
