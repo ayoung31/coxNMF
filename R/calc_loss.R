49ee599fe3377511440ac48f4762c17c6b18ebad
ayoung31 <54555484+ayoung31@users.noreply.github.com>
2024-05-10 16:15:04 -0400

    Try without scaling

--- R/calc_loss.R
+++ R/calc_loss.R
@@ -2,7 +2,7 @@
 calc_loss <- function(X,M,W,H,beta,alpha,y,delta,lambda,eta,WtX){
   N <- ncol(H)
   beta <- matrix(beta,ncol=1)
-  nmf_loss <- (norm(M*(X-W%*%H),'F')^2)/sum(M)
+  nmf_loss <- (norm(M*(X-W%*%H),'F')^2)
   
   if(WtX){
     a1 <- t(beta)%*%t(W)%*%(M*X)
@@ -14,7 +14,7 @@ calc_loss <- function(X,M,W,H,beta,alpha,y,delta,lambda,eta,WtX){
   for(i in 1:N){
     ind[,i] = (y>=y[i])^2
   }
-  surv_loss = 2*sum(delta*(a1 - log(exp(a1)%*%ind)))/N
+  surv_loss = sum(delta*(a1 - log(exp(a1)%*%ind)))
 
   penalty <- lambda*((1-eta)*sum(beta^2)/2 + eta*sum(abs(beta)))
   loss <- nmf_loss - alpha*(surv_loss - penalty)
