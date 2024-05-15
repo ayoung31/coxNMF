8b09bc427c0542d5659c1dcdbaadc07eb8c11bf6
ayoung31 <54555484+ayoung31@users.noreply.github.com>
2024-05-15 13:13:27 -0400

    bug take 2

--- R/recommend_alpha.R
+++ R/recommend_alpha.R
@@ -9,7 +9,8 @@ recommend_alpha <- function(X,M,y,delta,k,nalpha,maxit=15,WtX,eta=0,lambda=0, no
   W0 <- matrix(runif(p*k,0,1),nrow=p)
   M <- matrix(1,nrow=p,ncol=n)
   beta0 <- rep(0,k)
-  fit0 <- optimize_loss(X=X,M=M,H0=H0,W0=W0,beta0=beta0,y=y,delta=delta,alpha=0,maxit=maxit,WtX=WtX,eta=eta,lambda=lambda)
+  fit0 <- optimize_loss(X=X,M=M,H0=H0,W0=W0,beta0=beta0,y=y,delta=delta,alpha=0,
+                        maxit=maxit,WtX=WtX,eta=eta,lambda=lambda, norm.type=norm.type)
   if(fit0$loss$nmf_loss > fit0$loss$surv_loss){
     alpha5050 <- -1 * fit0$loss$nmf_loss / fit0$loss$surv_loss
   }else{
