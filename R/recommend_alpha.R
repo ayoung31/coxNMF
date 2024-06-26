c657259e00e368516922d125344ab89733c8980c
ayoung31 <54555484+ayoung31@users.noreply.github.com>
2024-06-26 17:14:32 -0400

    update recommend alpha to call cpp

--- R/recommend_alpha.R
+++ R/recommend_alpha.R
@@ -9,8 +9,8 @@ recommend_alpha <- function(X,M,y,delta,k,nalpha,maxit=15,WtX,eta=0,lambda=0, no
   W0 <- matrix(runif(p*k,0,1),nrow=p)
   M <- matrix(1,nrow=p,ncol=n)
   beta0 <- rep(0,k)
-  fit0 <- optimize_loss(X=X,M=M,H0=H0,W0=W0,beta0=beta0,y=y,delta=delta,alpha=0,
-                        maxit=maxit,WtX=WtX,eta=eta,lambda=lambda, norm.type=norm.type)
+  fit0 <- optimize_loss_cpp(X,M,H0,W0,beta0,y,delta,0,lambda,eta,1e-6,maxit,
+                            FALSE,WtX,norm.type,'lasso',TRUE)
   if(fit0$loss$nmf_loss > fit0$loss$surv_loss){
     alpha5050 <- -1 * fit0$loss$nmf_loss / fit0$loss$surv_loss
   }else{
@@ -21,6 +21,7 @@ recommend_alpha <- function(X,M,y,delta,k,nalpha,maxit=15,WtX,eta=0,lambda=0, no
   }
   grid <- 2:(nalpha/2 + 1)
   alpha_grid <- c(alpha5050*grid,alpha5050,alpha5050/grid)
-  
+  alpha_grid = alpha_grid[order(alpha_grid)]
+  alpha_grid = round(alpha_grid,3)
   return(list(alpha5050=alpha5050, alpha_grid=alpha_grid))
 }
