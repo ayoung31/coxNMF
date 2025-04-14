97ca8ae87330b00fb532e05b95469f10abfb1944
ayoung31 <ayoung31@ad.unc.edu>
2025-04-14 11:41:01 -0400

    update warm start

--- R/run_full_warm_start.R
+++ R/run_full_warm_start.R
@@ -1,8 +1,9 @@
 #' @export
 run_full_warm_start = function(X, y, delta, k, alpha, lambda = 0, eta = 0,
+                               lambdaW = 0, lambdaH = 0,
                     ninit = 100, imaxit = 30, maxit = 3000, tol = 1e-5, 
                     parallel = TRUE, ncore = NULL, replace = FALSE, 
-                    save = TRUE, verbose=TRUE, prefix){
+                    save = TRUE, verbose=TRUE, prefix, test=FALSE){
   
   X=as.matrix(X)
   
@@ -14,7 +15,8 @@ run_full_warm_start = function(X, y, delta, k, alpha, lambda = 0, eta = 0,
     ncore = detectCores() - 1
   }
   
-  params = set_param_grid(k=k, alpha=alpha, lambda=lambda, eta=eta, ninit=ninit, 
+  params = set_param_grid(k=k, alpha=alpha, lambda=lambda, eta=eta, 
+                          lambdaW=lambdaW, lambdaH=lambdaH, ninit=ninit, 
                           type="full", prefix=prefix,
                           ngene = ngene, maxit=maxit, tol=tol, imaxit=imaxit)
   
@@ -26,30 +28,42 @@ run_full_warm_start = function(X, y, delta, k, alpha, lambda = 0, eta = 0,
     parallel::clusterCall(cl, function(x) .libPaths(x), .libPaths())
   }
   
+  if(test){
+    comb = "list"
+  }else{
+    comb = "rbind"
+  }
+  
   metrics = 
     foreach(pa=1:nrow(params2), .inorder = FALSE, .errorhandling = 'remove', 
-            .combine = 'rbind', .packages = c("coxNMF","survival","cvwrapr")) %dopar% {
+            .combine = comb, .packages = c("coxNMF","survival","cvwrapr")) %dopar% {
     
     #a = params$alpha[pa]
     l = params$lambda[pa]
     e = params$eta[pa]
     k = params$k[pa]
+    lW = params$lambdaW[pa]
+    lH = params$lambdaH[pa]
     
     dat = list()
     i=1
     for(a in alpha){
-      file = params$file[params$alpha==a & params$lambda==l & params$eta==e & params$k==k]
+      file = params$file[params$alpha==a & params$lambda==l & 
+                           params$eta==e & params$k==k &
+                           params$lambdaW==lW & params$lambdaH==lH]
       if(replace | !file.exists(file)){
         print(sprintf('alpha=%f, lambda=%f, eta=%f, k=%d\n',a,l,e,k))
         print("running...")
-        if(a==0){
+        if(a==alpha[1]){
           fit_cox = run_coxNMF(X=X, y=y, delta=delta, k=k, 
                                alpha=a, lambda=l, eta=e, 
+                               lambdaW = lW, lambdaH = lH,
                                tol=tol, maxit=maxit, verbose=verbose,
                                ninit=ninit, imaxit=imaxit)
         }else{
           fit_cox = run_coxNMF(X=X, y=y, delta=delta, k=k, 
-                               alpha=a, lambda=l, eta=e, 
+                               alpha=a, lambda=l, eta=e,
+                               lambdaW = lW, lambdaH = lH,
                                tol=tol, maxit=maxit, verbose=verbose,
                                ninit=ninit, imaxit=imaxit,
                                W0=fit_cox$W, H0=fit_cox$H, beta0=fit_cox$beta)
@@ -64,7 +78,7 @@ run_full_warm_start = function(X, y, delta, k, alpha, lambda = 0, eta = 0,
       }
       
       if(fit_cox$`NaN flag`){
-        break
+        warning("alpha too large")
       }
       
       
@@ -73,7 +87,12 @@ run_full_warm_start = function(X, y, delta, k, alpha, lambda = 0, eta = 0,
       W = fit_cox$W
       H = fit_cox$H
       beta = fit_cox$beta
-      c = cvwrapr::getCindex(t(X) %*% W %*% beta, Surv(y, delta))
+      
+      if(all(is.nan(t(H) %*% beta))){
+        warning("alpha too large 2")
+      }
+      
+      c = cvwrapr::getCindex(t(H) %*% beta, Surv(y, delta))
       loss = fit_cox$loss
       ol = loss$loss
       sl = loss$surv_loss
@@ -82,16 +101,22 @@ run_full_warm_start = function(X, y, delta, k, alpha, lambda = 0, eta = 0,
       bic = -2*sl + k*log(ncol(X))
       converged=fit_cox$iter < maxit
       
-      dat[[i]] = data.frame(k=k,alpha=a,lambda=l,eta=e,c=c,loss=ol,sloss=sl,
-                 nloss=nl,pen=pen,bic=bic,converged=converged,niter=fit_cox$iter,
-                 flag_nan = fit_cox$`NaN flag`)
+      dat[[i]] = data.frame(k=k,alpha=a,lambda=l,eta=e, lambdaW=lW, lambdaH=lH,
+                            c=c,loss=ol,sloss=sl,nloss=nl,pen=pen,bic=bic,
+                            converged=converged,niter=fit_cox$iter,
+                            flag_nan = fit_cox$`NaN flag`)
       i=i+1
       
       
       
     }#end alpha loop
 
-    do.call('rbind',dat)
+    
+    if(test){
+      list(mets=dat[[1]],fit_cox=fit_cox)
+    }else{
+      do.call('rbind',dat)
+    }
   }#end foreach
   
   if(parallel){
